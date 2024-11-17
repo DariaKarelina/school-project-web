a2909e405835006fffc55d81ae300ff409228e69
daria karelina <166645643+DariaKarelina@users.noreply.github.com>
2024-11-17 17:26:31 +0300

    fb1InfoForFB.js
    new

--- src/fb1InfoForFB.js
+++ src/fb1InfoForFB.js
@@ -3,12 +3,11 @@ import "./fb1InfoForFB.css";
 const InfoForFB = () => {
     return(
         <div className="info">
-          <textarea className="users-name"></textarea>  
-          <textarea className="email"></textarea>
-          <textarea className="msg"></textarea>
-          <button className="send">Оправить</button>
-
-
+            <div className="head-fb">Обратная связь</div>
+            <textarea className="users-name"></textarea>  
+            <textarea className="email"></textarea>
+            <textarea className="msg"></textarea>
+            <button className="send">Оправить</button>
         </div>
 
     );
