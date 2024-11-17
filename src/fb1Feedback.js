bd73c4c79b5a09b2713b4937066d53ef4fa50403
daria karelina <166645643+DariaKarelina@users.noreply.github.com>
2024-11-17 17:20:18 +0300

    fb1Feedback.js
    new

--- src/fb1Feedback.js
+++ src/fb1Feedback.js
@@ -1,13 +1,11 @@
 import "./fb1Feedback.css";
 import Menu from "./Menu";
 import TextAndFeedback from "./fb1TextAndFeedback";
-import Headers from "./fb1Headers";
 
 const Feedback = () => {
     return(
         <div className="feedback">
             <Menu/>
-            <Headers/>
             <TextAndFeedback/>
 
         </div>
