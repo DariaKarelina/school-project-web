35d1955cc2e357b8cd298a4f88ebfdea3c311b20
daria karelina <166645643+DariaKarelina@users.noreply.github.com>
2024-11-26 06:32:50 +0300

    Add files via upload

---
+++ src/Pages/Home/News/News3/home1NewsThree.js
@@ -0,0 +1,10 @@
+import './home1NewsThree.css';
+const NewsThree = () => {
+    return(
+        <div className="news3">
+            <div className='news3'>three</div>
+        </div> 
+    )
+}
+
+export default NewsThree;
