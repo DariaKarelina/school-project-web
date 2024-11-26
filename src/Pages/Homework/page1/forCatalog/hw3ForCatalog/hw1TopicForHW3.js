75a6684afde8f0a02979aabdde6dcbbdac394f0d
daria karelina <166645643+DariaKarelina@users.noreply.github.com>
2024-11-26 06:33:47 +0300

    Add files via upload

---
+++ src/Pages/Homework/page1/forCatalog/hw3ForCatalog/hw1TopicForHW3.js
@@ -0,0 +1,18 @@
+import "./hw1TopicForHW3.css";
+
+const TopicForHW3=()=>{
+    return(
+        <div className="topic-for-hw3">
+            <a  href='/homework3'>
+                <div className="topic-hw3">
+                    тема дз3
+                </div>
+                <div className="deadline-hw3">
+                    дедлайн дз3
+                </div>
+            </a>
+        </div>
+    );
+}
+
+export default TopicForHW3;
